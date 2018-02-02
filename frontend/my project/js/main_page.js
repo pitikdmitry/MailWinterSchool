97b7ab5c542be1eba00fbc3495b70a1407e7dec9
pitikdmitry <pitik.dmitry@mail.ru>
2018-02-02 12:27:32 +0300

    changed structure

--- frontend/my project/js/main_page.js
+++
@@ -1,9 +0,0 @@
-const registrationButton = document.getElementById('Registration-button');
-const mainPart = document.getElementById('main');
-const registrationForm = document.getElementById('registration-form');
-registrationButton.addEventListener('click', function (event) {
-    mainPart.hidden = true;
-    registrationForm.hidden = true;
-});
-
-
