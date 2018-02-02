4c6b278a8d2f9e53d0ec1d1a53d50cfa0f46da2e
pitikdmitry <pitik.dmitry@mail.ru>
2018-02-02 15:13:38 +0300

    some small changes, i decided not to use handmade tree building, i will use only templates;

--- frontend/my project/js/main.js
+++ frontend/my project/js/main.js
@@ -1,36 +1,73 @@
 const http = window.Http;
 const Block = window.Block;
 
-// big sections for hiding
-const mainSection = new Block(document.getElementById('main_page'));
-const registrationSection = new Block(document.getElementById('registration'));
-const signInSection = new Block(document.getElementById('signIn'));
-const playerProfileSection = new Block(document.getElementById('player-profile'));
-const playersRatingSection = new Block(document.getElementById('players-rating'));
+const sections = {
+    mainSection: new Block(document.getElementById('main_page')),
+    registrationSection: new Block(document.getElementById('registration')),
+    signInSection: new Block(document.getElementById('signIn')),
+    playerProfileSection: new Block(document.getElementById('player-profile')),
+    playersRatingSection: new Block(document.getElementById('players-rating'))
+};
 
-
-// navigation buttons
-const registrationButton = new Block(document.getElementById('registration-btn'));
-const signInButton = new Block(document.getElementById('signIn-btn'));
-
-// navigation buttons for big screen
-const tournamentsButton = new Block(document.getElementById('tournament-btn'));
-const gamesButton = new Block(document.getElementById('games-btn'));
-const teamsButton = new Block(document.getElementById('teams-btn'));
-const playersButton = new Block(document.getElementById('players-btn'));
-
-
-// navigation buttons for small screen FULL
-
-// navigation buttons for small screen Small Menu
-debugger;
 // preload
 hideAllSections();
-mainSection.show();
+sections.mainSection.show();
 
-registrationButton.on('click', openRegistration);
-signInButton.on('click', openSignIn);
-tournamentsButton.on('click', openTournaments);
-gamesButton.on('click', openGames);
-teamsButton.on('click', openTeams);
-playersButton.on('click', openPlayers);
+// navigation buttons for big screen
+const bigScreenButtons = {
+    registrationButton: new Block(document.getElementById('registration-btn')),
+    signInButton: new Block(document.getElementById('signIn-btn')),
+
+    tournamentsButton: new Block(document.getElementById('tournament-btn')),
+    gamesButton: new Block(document.getElementById('games-btn')),
+    teamsButton: new Block(document.getElementById('teams-btn')),
+    playersButton: new Block(document.getElementById('players-btn'))
+};
+
+// navigation buttons for small screen FULL
+const smallScreenAlButtons = {
+    registrationButton: bigScreenButtons.registrationButton,
+    signInButton: bigScreenButtons.signInButton
+};
+// navigation buttons for small screen Small Menu
+
+
+bigScreenButtons.registrationButton.on('click', openRegistration);
+bigScreenButtons.signInButton.on('click', openSignIn);
+bigScreenButtons.tournamentsButton.on('click', openTournaments);
+bigScreenButtons.gamesButton.on('click', openGames);
+bigScreenButtons.teamsButton.on('click', openTeams);
+bigScreenButtons.playersButton.on('click', openPlayers);
+
+function openRegistration() {
+    hideAllSections();
+    sections.registrationSection.show();
+}
+
+function openSignIn() {
+    hideAllSections();
+    sections.signInSection.show();
+}
+
+function openTournaments() {
+    hideAllSections();
+}
+
+function openGames() {
+    hideAllSections();
+}
+
+function openTeams() {
+    hideAllSections();
+}
+
+function openPlayers() {
+    hideAllSections();
+}
+
+function hideAllSections() {
+
+    for (let [key, value] of Object.entries(sections)) {
+        sections[key].hide();
+    }
+}
--- frontend/my project/js/openFunctions.js
+++ frontend/my project/js/openFunctions.js
@@ -1,39 +1,8 @@
 (function() {
     'use strict';
 
-    function openRegistration() {
-        hideAllSections();
-        registrationSection.show();
-    }
 
-    function openSignIn() {
-        hideAllSections();
-        signInSection.show();
-    }
 
-    function openTournaments() {
-        hideAllSections();
-    }
-
-    function openGames() {
-        hideAllSections();
-    }
-
-    function openTeams() {
-        hideAllSections();
-    }
-
-    function openPlayers() {
-        hideAllSections();
-    }
-
-    function hideAllSections() {
-        mainSection.hide();
-        registrationSection.hide();
-        signInSection.hide();
-        playerProfileSection.hide();
-        playersRatingSection.hide();
-    }
 
     window.openRegistration = openRegistration;
     window.openSignIn = openSignIn;
